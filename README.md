84c43fc7557849810c2423f4c9b1516bdaf2d6cf
Stian Didriksen <stian@Helene-Didriksens-Macbook.local>
2010-02-21 12:04:23 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Element.Style.Transform
+================
+
+Provides a cross browser way of letting you use the CSS3 transform property. Inspired by http://github.com/zachstronaut/jquery-css-transform
+
+
+
+How to Use
+----------
+
+Coming shortly
---
+++ Source/Element.Style.Transform.js
@@ -0,0 +1,19 @@
+/*
+---
+
+script: Element.Style.Transform.js
+
+description: Provides a cross browser way of letting you use the CSS3 transform property. Inspired by http://github.com/zachstronaut/jquery-css-transform
+
+license: MIT-style license
+
+authors: Stian Didriksen
+
+requires:
+- core:1.11/String
+- core:1.11/Array
+
+provides: [Element.Style.Transform]
+
+...
+*/
