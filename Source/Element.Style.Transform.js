b9a9a87c06eb8ccd81f71688ab43895aeb59c52c
Stian Didriksen <stian@Helene-Didriksens-Macbook.local>
2010-02-21 14:05:14 +0100

    Adding tests. Working in its basics.

--- Source/Element.Style.Transform.js
+++ Source/Element.Style.Transform.js
@@ -10,12 +10,39 @@ license: MIT-style license
 authors: Stian Didriksen
 
 requires:
-- core:1.2.4/String
-- core:1.2.4/Array
-- more:1.2.4/Class.Refactor
+- core:1.2.4/Browser
+- core:1.2.4/Element
 
 provides: [Element.Style.Transform]
 
 ...
 */
 
+Element.Properties.transform = {
+
+	set: function(transform){
+		var property = 'transform';
+		if(Browser.Engine.webkit) property = 'WebkitTransform';
+		if(Browser.Engine.gecko)  property = 'MozTransform';
+		
+		this.style[property] = transform;
+		this.store('transform', transform);
+	},
+
+	get: function(){
+		return this.retrieve('transform', 1);
+	}
+
+};
+
+Element.implement({
+
+	setTransform: function(value){
+		return this.set('transform', value);
+	},
+
+	getTransform: function(){
+		return this.get('transform');
+	}
+
+});
