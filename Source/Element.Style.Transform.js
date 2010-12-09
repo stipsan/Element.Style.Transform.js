372ee916422ce9fcf69bcfaff0614849b974758a
amadeus <amadeusdemarzi@gmail.com>
2010-12-08 17:25:15 -0800

    * Updating to support Moo 1.3 * Added support for Transition property as well

--- Source/Element.Style.Transform.js
+++ Source/Element.Style.Transform.js
@@ -3,29 +3,33 @@
 
 script: Element.Style.Transform.js
 
-description: Provides a cross browser way of letting you use the CSS3 transform property. Inspired by http://github.com/zachstronaut/jquery-css-transform
+description:
+	Provides a cross browser way of letting you use the CSS3 transform and transition properties.
+	Inspired by http://github.com/zachstronaut/jquery-css-transform
 
 license: MIT-style license
 
 authors: Stian Didriksen
 
-requires:
-- core:1.2.4/Browser
-- core:1.2.4/Element
+contributers: Amadeus Demarzi
 
-provides: [Element.Style.Transform]
+requires:
+- core:1.3/Browser
+
+provides: [Element.Style.Transform, Element.Style.Transition]
 
 ...
 */
-
 Element.Properties.transform = {
-
 	set: function(transform){
 		var property = 'transform';
-		if(Browser.Engine.webkit) property = 'WebkitTransform';
-		if(Browser.Engine.gecko)  property = 'MozTransform';
-		if(Browser.Engine.presto) property = 'OTransform';
-		
+		if(Browser.safari || Browser.chrome || Browser.Platform.ios)
+			property = 'WebkitTransform';
+		if(Browser.firefox4)
+			property = 'MozTransform';
+		if(Browser.opera)
+			property = 'OTransform';
+
 		this.style[property] = transform;
 		this.store('transform', transform);
 	},
@@ -33,17 +37,41 @@ Element.Properties.transform = {
 	get: function(){
 		return this.retrieve('transform', '');
 	}
+};
 
+Element.Properties.transition = {
+	set: function(transition){
+		var property = 'transition';
+		if(Browser.safari || Browser.chrome || Browser.Platform.ios)
+			property = 'WebkitTransition';
+		if(Browser.firefox4)
+			property = 'MozTransition';
+		if(Browser.opera)
+			property = 'OTransition';
+
+		this.style[property] = transition;
+		this.store('transition', transition);
+	},
+
+	get: function(){
+		return this.retrieve('transition', '');
+	}
 };
 
 Element.implement({
-
 	setTransform: function(value){
 		return this.set('transform', value);
 	},
 
 	getTransform: function(){
 		return this.get('transform');
-	}
+	},
 
+	setTransition: function(value){
+		return this.set('transition', value);
+	},
+
+	getTransition: function(){
+		return this.get('transition');
+	}
 });
